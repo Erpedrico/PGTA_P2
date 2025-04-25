ae3c056b7933d96bf5676188229ecbae0a9aaefa
Linfei Han <linfeihan844@gmail.com>
2025-04-25 15:46:33 +0200

    enable data item 10, ignore not needed camps data item 10 is finished

--- functions/data_item_functions/data_item_10.py
+++ functions/data_item_functions/data_item_10.py
@@ -1,14 +1,15 @@
 """
 Data Item I048/250, BDS Register Data
+"""
 
 import data_item_10_subdecoder as BDS
 
 
 def data_item_10(packet) -> (list | None):
-    
+    """
     input: [REP(1byte), [[BDS(7bytes), BDS1(4bit), BDS2(4bit)]...]]
-    output: [REP(1byte), [BDS(decoded as dict),...]]
-    
+    output: [REP(1byte), [BDS(decoded as dict), ...]]
+    """
     cleaned_packet = "".join(
         c for c in packet if c in "0123456789abcdefABCDEF")
     if len(cleaned_packet) % 2 != 0:
@@ -35,11 +36,10 @@ def data_item_10(packet) -> (list | None):
     for i in range(0, len(packet_bytes), 8):
         bdsdata = packet_bytes[i:i+7]           # bytes
         bds12 = packet_bytes[i+7]
-                # Tables are numbered A-2-X where “X” is the decimal equivalent of the BDS code Y/Z. Y is the BDS1 code and Z is the BDS2 code used to access the data format for a particular register. 
+        # Tables are numbered A-2-X where “X” is the decimal equivalent of the BDS code Y/Z. Y is the BDS1 code and Z is the BDS2 code used to access the data format for a particular register.
         bdsdata_decoded = BDS.decode(bdsdata, bds12)
         if bdsdata_decoded is None:
-            print("Error al decodificar BDS")
+            # not needed subcamp, just ignore
             continue
         bds.append(bdsdata)
     return [rep, bds]
-"""

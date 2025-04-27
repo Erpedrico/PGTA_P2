9b5a1116ef5f8812abc2164e3f1898893b58cd48
Linfei Han <linfeihan844@gmail.com>
2025-04-27 19:01:57 +0200

    optimizar funcionalidad de pruebas

--- Pruebas.py
+++ Pruebas.py
@@ -23,207 +23,49 @@ from functions.data_item_functions.data_item_11Corrected import data_item_11
 from functions.data_item_functions.data_item_12Corrected import data_item_12
 from functions.data_item_functions.data_item_13Corrected import data_item_13
 from functions.data_item_functions.data_item_14Corrected import data_item_14
-from functions.data_item_functions.data_item_8Corrected import data_item_8
 # from functions.data_item_functions.data_item_200 import data_item_200
 # from functions.data_item_functions.data_item_170 import data_item_170
 
 
+def single_test(func: callable, hexnumber: str):
+    print(f"Prueba de {func.__name__} con el número hexadecimal: {hexnumber}")
+    sigma = func(hexnumber)
+    if sigma is None:
+        print(f"falla la prueba de {func.__name__}")
+    else:
+        print(sigma)
+    print("--------------------\n")
+
+
 def run_all_tests():
 
-    hexnumber_1 = "A1D2"
-    print("Prueba_1")
-    sigma = data_item_1(hexnumber_1)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_2 = "A2D3"
-    print("Prueba_2 EXT")
-    sigma = data_item_2(hexnumber_2)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_3 = "210F"
-    print("Prueba_3 EXT")
-    sigma = data_item_3(hexnumber_3)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_4 = "01000000"
-    print("Prueba_4")
-    sigma = data_item_4(hexnumber_4)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_5 = "0080FF80"
-    print("Prueba_5")
-    sigma = data_item_5(hexnumber_5)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_6 = "600F"
-    print("Prueba_6")
-    sigma = data_item_6(hexnumber_6)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_7 = "A5"
-    print("Prueba_7")
-    sigma = data_item_7(hexnumber_7)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_8 = "ABCDEF"
-    print("Prueba_8Correcto")
-    sigma = data_item_8(hexnumber_8)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-
-    hexnumber_9 = "3452ABC61192"
-    print("Prueba_9")
-    sigma = data_item_9(hexnumber_9)
-    for entry in sigma:
-        print(entry)
-
-    hexnumber_10 = "600F"
-    print("Prueba_10")
-    sigma = data_item_10(hexnumber_10)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_12 = "8E23"
-    print("Prueba_12")
-    sigma = data_item_12(hexnumber_12)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_13 = "A512345678"
-    print("Prueba_13")
-    sigma = data_item_13(hexnumber_13)
-    for entry in sigma:
-        print(entry)
-    print("\n")
-
-    hexnumber_14 = "123456"
-    print("Prueba_14")
-    sigma = data_item_14(hexnumber_14)
-    for entry in sigma:
-        print(entry)
-
-    """
-    hexnumber_200="12345678"
-    print("Prueba_200")
-    sigma = data_item_200(hexnumber_200)
-    for entry in sigma:
-        print(entry)
-    print("\n")   
-
-    hexnumber_170="A5B2"
-    print("Prueba_170")
-    sigma = data_item_170(hexnumber_170)
-    for entry in sigma:
-        print(entry)
-    print("\n")   
-    """
-    hexnumber_6="1A2B"
-    print("Prueba_6")
-    sigma = data_item_6(hexnumber_6)
-    for entry in sigma:
-        print(entry)
-    print("\n") 
-
-
-
-    hexnumber_15 = str(11111111)
-    print("Prueba_15")
-    sigma = data_item_15(hexnumber_15)
-    for entry in sigma:
-        print(entry)
-    print("\n")  
-
-    hexnumber_16 = str(11011213)
-    print("Prueba_16")
-    mensajes = data_item_16(hexnumber_16)
-    for entry in mensajes:
-        print(entry)
-    print("\n") 
-
-    # PREGUNTAR EL 17 SI HAY QUE ESPECIFICAR LOS CODIGOS
-    hexnumber_17 = "1111"
-    print("Prueba_17")
-    code_mode3A = data_item_17(hexnumber_17)
-    for entry in code_mode3A:
-        print(entry)
-    print (code_mode3A)
-    print("\n") 
-
-    # PREGUNTAR EL 18 SI HAY QUE ESPECIFICAR LOS CODIGOS Y INTENTAR ENTENDER LA ESTRUCTURA
-
-
-    hexnumber_19 = "1111"
-    print("Prueba_19")
-    height_3D = data_item_19(hexnumber_19)
-    print (height_3D)
-    print("\n") 
-
-    # PREGUNTAR EL 20 PARA QUE SIRVE EL FX DEL PRIMER OCTECT, ES DECIR, PARA QUE QUERRIAS ALARGAR ESTE FIELD?
-
-    hexnumber_21 = "1111"
-    print("Prueba_21")
-    messages = data_item_21(hexnumber_21)
-    for entry in messages:
-        print(entry)
-    print (messages)
-    print("\n") 
-
-    hexnumber_22 = "11111111111111"
-    print("Prueba_22")
-    ACASRA = data_item_22(hexnumber_22)
-    print (ACASRA)
-    print("\n") 
-
-    hexnumber_23 = "11"
-    print("Prueba_23")
-    messages = data_item_23(hexnumber_23)
-    for entry in messages:
-        print(entry)
-    print (messages)
-    print("\n") 
-
-    hexnumber_24 = "1111"
-    print("Prueba_24")
-    messages = data_item_24(hexnumber_24)
-    for entry in messages:
-        print(entry)
-    print (messages)
-    print("\n") 
-
-    hexnumber_25 = "11"
-    print("Prueba_25")
-    messages = data_item_25(hexnumber_25)
-    for entry in messages:
-        print(entry)
-    print (messages)
-    print("\n") 
-
-    hexnumber_26 = "1111"
-    print("Prueba_26")
-    messages = data_item_26(hexnumber_26)
-    for entry in messages:
-        print(entry)
-    print (messages)
-    print("\n") 
+    single_test(data_item_1, "A1D2")
+    single_test(data_item_2, "A2D3")
+    single_test(data_item_3, "210F")
+    single_test(data_item_4, "01000000")
+    single_test(data_item_5, "0080FF80")
+    single_test(data_item_6, "600F")
+    single_test(data_item_7, "0000")
+    single_test(data_item_8, "ABCDEF")
+    single_test(data_item_9, "3452ABC61192")
+    single_test(data_item_10, "600F")
+    single_test(data_item_11, "0000")
+    single_test(data_item_12, "8E23")
+    single_test(data_item_13, "A512345678")
+    single_test(data_item_14, "123456")
+    single_test(data_item_15, "11111111")
+    single_test(data_item_16, "11011213")
+    single_test(data_item_17, "1111")
+    single_test(data_item_19, "1111")
+    single_test(data_item_21, "1111")
+    single_test(data_item_22, "11111111111111")
+    single_test(data_item_23, "11")
+    single_test(data_item_24, "1111")
+    single_test(data_item_25, "11")
+    single_test(data_item_26, "1111")
+    single_test(data_item_28, "0000")
 
 if __name__ == "__main__":
     run_all_tests()
-    #comment to run some individual test
+    # comment to run some individual test like:
+    # single_test(data_item_1, "A1D2")
